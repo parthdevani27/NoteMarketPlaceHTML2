8e2db956184007647071b306453deb04fb691126
PARTH <parthdevani2121@gmail.com>
2021-02-11 17:00:54 +0530

    commit5

---
+++ Database/database1.sql
@@ -0,0 +1,470 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 11, 2021 at 08:32 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `CountryCode` varchar(100) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `downloads`
+--
+
+CREATE TABLE `downloads` (
+  `ID` int(11) NOT NULL,
+  `NoteID` int(11) NOT NULL,
+  `Seller` int(11) NOT NULL,
+  `Downloader` int(11) NOT NULL,
+  `IsSellerHasAllowedDownload` bit(1) NOT NULL DEFAULT b'0',
+  `AttachmentPath` varchar(1000) DEFAULT NULL,
+  `IsAttachmentDownloaded` bit(1) NOT NULL DEFAULT b'0',
+  `AttachmentDownloadedDate` datetime DEFAULT NULL,
+  `IsPaid` bit(50) NOT NULL,
+  `PurchasedPrice` decimal(10,0) DEFAULT NULL,
+  `NoteTitle` varchar(100) NOT NULL,
+  `NoteCategory` varchar(100) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notecategories`
+--
+
+CREATE TABLE `notecategories` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` varchar(1000) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notetypes`
+--
+
+CREATE TABLE `notetypes` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` varchar(1000) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `referencedata`
+--
+
+CREATE TABLE `referencedata` (
+  `ID` int(11) NOT NULL,
+  `Value` varchar(100) NOT NULL,
+  `Datavalue` varchar(100) NOT NULL,
+  `RefCategory` varchar(100) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellernotes`
+--
+
+CREATE TABLE `sellernotes` (
+  `ID` int(11) NOT NULL,
+  `SellerID` int(11) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `ActionedBy` int(11) DEFAULT NULL,
+  `AdminRemarks` varchar(1000) DEFAULT NULL,
+  `PublishedDate` datetime DEFAULT NULL,
+  `Title` varchar(100) NOT NULL,
+  `Category` int(11) NOT NULL,
+  `DisplayPicture` varchar(500) DEFAULT NULL,
+  `NoteType` int(11) DEFAULT NULL,
+  `NumberofPages` int(11) DEFAULT NULL,
+  `Description` varchar(1000) NOT NULL,
+  `UniversityName` varchar(200) DEFAULT NULL,
+  `Country` int(11) DEFAULT NULL,
+  `Course` varchar(100) DEFAULT NULL,
+  `CourseCode` varchar(100) DEFAULT NULL,
+  `Professor` varchar(100) DEFAULT NULL,
+  `IsPaid` bit(1) NOT NULL DEFAULT b'0',
+  `SellingPrice` decimal(10,0) DEFAULT NULL,
+  `NotesPreview` varchar(10000) DEFAULT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellernotesattachements`
+--
+
+CREATE TABLE `sellernotesattachements` (
+  `ID` int(11) NOT NULL,
+  `NoteID` int(11) NOT NULL,
+  `FileName` varchar(100) NOT NULL,
+  `FilePath` varchar(1000) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellernotesreportedissues`
+--
+
+CREATE TABLE `sellernotesreportedissues` (
+  `ID` int(11) NOT NULL,
+  `NoteID` int(11) NOT NULL,
+  `ReportedBYID` int(11) NOT NULL,
+  `AgainstDownloadID` int(11) NOT NULL,
+  `Remarks` varchar(1000) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellernotesreviews`
+--
+
+CREATE TABLE `sellernotesreviews` (
+  `ID` int(11) NOT NULL,
+  `NoteID` int(11) NOT NULL,
+  `ReviewedByID` int(11) NOT NULL,
+  `AgainstDownloadsID` int(11) NOT NULL,
+  `Ratings` decimal(10,0) NOT NULL,
+  `Comments` varchar(1000) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `systemconfigurations`
+--
+
+CREATE TABLE `systemconfigurations` (
+  `ID` int(11) NOT NULL,
+  `Key1` varchar(100) NOT NULL,
+  `Value` varchar(1000) NOT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userprofile`
+--
+
+CREATE TABLE `userprofile` (
+  `ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `DOB` datetime DEFAULT NULL,
+  `Gender` int(11) DEFAULT NULL,
+  `SecondaryEmailAddress` varchar(100) DEFAULT NULL,
+  `Phonenumber–CountryCode` varchar(50) NOT NULL,
+  `Phonenumber` varchar(20) NOT NULL,
+  `ProfilePicture` varchar(500) DEFAULT NULL,
+  `AddressLine1` varchar(100) NOT NULL,
+  `AddressLine2` varchar(100) NOT NULL,
+  `City` varchar(50) NOT NULL,
+  `State` varchar(50) NOT NULL,
+  `ZipCode` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL,
+  `University` varchar(100) DEFAULT NULL,
+  `College` varchar(100) DEFAULT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userroles`
+--
+
+CREATE TABLE `userroles` (
+  `RoleID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Description` varchar(1000) DEFAULT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `RoleID` int(11) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `EmailID` varchar(100) NOT NULL,
+  `Password` varchar(24) NOT NULL,
+  `IsEmailVerified` bit(1) NOT NULL DEFAULT b'0',
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `IsActive` bit(1) NOT NULL DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `downloads`
+--
+ALTER TABLE `downloads`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `NoteID` (`NoteID`),
+  ADD KEY `Seller` (`Seller`),
+  ADD KEY `Downloader` (`Downloader`);
+
+--
+-- Indexes for table `notecategories`
+--
+ALTER TABLE `notecategories`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `notetypes`
+--
+ALTER TABLE `notetypes`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `referencedata`
+--
+ALTER TABLE `referencedata`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Datavalue` (`Datavalue`);
+
+--
+-- Indexes for table `sellernotes`
+--
+ALTER TABLE `sellernotes`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `sellernotes_ibfk_1` (`SellerID`),
+  ADD KEY `sellernotes_ibfk_2` (`ActionedBy`),
+  ADD KEY `sellernotes_ibfk_3` (`Category`),
+  ADD KEY `sellernotes_ibfk_4` (`NoteType`),
+  ADD KEY `sellernotes_ibfk_5` (`Country`),
+  ADD KEY `sellernotes_ibfk_6` (`Status`);
+
+--
+-- Indexes for table `sellernotesattachements`
+--
+ALTER TABLE `sellernotesattachements`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `NoteID` (`NoteID`);
+
+--
+-- Indexes for table `sellernotesreportedissues`
+--
+ALTER TABLE `sellernotesreportedissues`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `NoteID` (`NoteID`),
+  ADD KEY `ReportedBYID` (`ReportedBYID`),
+  ADD KEY `sellernotesreportedissues_ibfk_3` (`AgainstDownloadID`);
+
+--
+-- Indexes for table `sellernotesreviews`
+--
+ALTER TABLE `sellernotesreviews`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `NoteID` (`NoteID`),
+  ADD KEY `ReviewedByID` (`ReviewedByID`),
+  ADD KEY `sellernotesreviews_ibfk_3` (`AgainstDownloadsID`);
+
+--
+-- Indexes for table `systemconfigurations`
+--
+ALTER TABLE `systemconfigurations`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `userprofile`
+--
+ALTER TABLE `userprofile`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `users_ibfk_2` (`Gender`),
+  ADD KEY `users_ibfk_3` (`Phonenumber–CountryCode`);
+
+--
+-- Indexes for table `userroles`
+--
+ALTER TABLE `userroles`
+  ADD PRIMARY KEY (`RoleID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `EmailID` (`EmailID`),
+  ADD KEY `RoleID` (`RoleID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `downloads`
+--
+ALTER TABLE `downloads`
+  ADD CONSTRAINT `downloads_ibfk_1` FOREIGN KEY (`NoteID`) REFERENCES `sellernotes` (`ID`),
+  ADD CONSTRAINT `downloads_ibfk_2` FOREIGN KEY (`Seller`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `downloads_ibfk_3` FOREIGN KEY (`Downloader`) REFERENCES `users` (`ID`);
+
+--
+-- Constraints for table `sellernotes`
+--
+ALTER TABLE `sellernotes`
+  ADD CONSTRAINT `sellernotes_ibfk_1` FOREIGN KEY (`SellerID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `sellernotes_ibfk_2` FOREIGN KEY (`ActionedBy`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `sellernotes_ibfk_3` FOREIGN KEY (`Category`) REFERENCES `notecategories` (`ID`),
+  ADD CONSTRAINT `sellernotes_ibfk_4` FOREIGN KEY (`NoteType`) REFERENCES `notetypes` (`ID`),
+  ADD CONSTRAINT `sellernotes_ibfk_5` FOREIGN KEY (`Country`) REFERENCES `countries` (`ID`),
+  ADD CONSTRAINT `sellernotes_ibfk_6` FOREIGN KEY (`Status`) REFERENCES `referencedata` (`ID`);
+
+--
+-- Constraints for table `sellernotesattachements`
+--
+ALTER TABLE `sellernotesattachements`
+  ADD CONSTRAINT `sellernotesattachements_ibfk_1` FOREIGN KEY (`NoteID`) REFERENCES `sellernotes` (`ID`);
+
+--
+-- Constraints for table `sellernotesreportedissues`
+--
+ALTER TABLE `sellernotesreportedissues`
+  ADD CONSTRAINT `sellernotesreportedissues_ibfk_1` FOREIGN KEY (`NoteID`) REFERENCES `sellernotes` (`ID`),
+  ADD CONSTRAINT `sellernotesreportedissues_ibfk_2` FOREIGN KEY (`ReportedBYID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `sellernotesreportedissues_ibfk_3` FOREIGN KEY (`AgainstDownloadID`) REFERENCES `downloads` (`ID`);
+
+--
+-- Constraints for table `sellernotesreviews`
+--
+ALTER TABLE `sellernotesreviews`
+  ADD CONSTRAINT `sellernotesreviews_ibfk_1` FOREIGN KEY (`NoteID`) REFERENCES `sellernotes` (`ID`),
+  ADD CONSTRAINT `sellernotesreviews_ibfk_2` FOREIGN KEY (`ReviewedByID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `sellernotesreviews_ibfk_3` FOREIGN KEY (`AgainstDownloadsID`) REFERENCES `downloads` (`ID`);
+
+--
+-- Constraints for table `userprofile`
+--
+ALTER TABLE `userprofile`
+  ADD CONSTRAINT `userprofile_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`Gender`) REFERENCES `referencedata` (`ID`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`RoleID`) REFERENCES `userroles` (`RoleID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
